41137405e27b766259e7621043914fffcb17f447
Zonaisha Fatima <fatimazonaisha@gmail.com>
2025-04-25 12:48:21 +0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Number1"));
+console.log(num1);
+let num2 = parseInt(prompt('Number2'));
+console.log(num2);
+sum = num1 + num2;
+sub = num1 - num2;
+mul = num1 * num2;
+div = num1 / num2;
+console.log('Addition = '+sum+ '\nSubtraction = '+sub+ '\nMultiplication = '+mul+ '\nDivision = '+div);
+
+
